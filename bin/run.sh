9ac1fb82cef5d3253c504add11e8ee795496a744
DeVerl Stokes <deverl.s@sigmasense.com>
2025-04-07 14:28:21 -0600

    Modified to use port 80 on linux.

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-flask --app fccapp run --host 0.0.0.0 --port 4000
+if [ "$(uname)" = "Darwin" ]
+then
+    flask --app fccapp run --host 0.0.0.0 --port 4000
+else
+    flask --app fccapp run --port 80
+fi
 
