9c622acbad2df8f7b5b4bc5788f47b22deac28c6
DeVerl Stokes <deverl.s@sigmasense.com>
2025-04-07 14:31:15 -0600

    Fixed run params.

--- bin/run.sh
+++ bin/run.sh
@@ -2,8 +2,8 @@
 
 if [ "$(uname)" = "Darwin" ]
 then
-    flask --app fccapp run --host 0.0.0.0 --port 4000
+    flask --app fccapp run --port 4000
 else
-    flask --app fccapp run --port 80
+    flask --app fccapp run --host 0.0.0.0 --port 80
 fi
 
