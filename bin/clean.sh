42875af38093df00fa13e37699ca2705487c3fcf
DeVerl Stokes <deverl.s@sigmasense.com>
2025-04-07 12:29:29 -0600

    Helper scripts.

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-rm -f *.dat *.csv l_amat.zip counts fcculs.db
+CLEAN_ALL=0
 
+while [ $# -gt 0 ]
+do
+    if [ "$1" = "-a" -o "$1" = "--clean-all" ]
+    then
+        CLEAN_ALL=1
+    else
+        echo "Unrecognized argument: $1"
+        exit 1
+    fi
+    shift
+done
 
+rm -f *.dat *.csv counts fcculs.db
+
+if [ $CLEAN_ALL -gt 0 ]
+then
+    rm -f l_amat.zip
+fi
---
+++ bin/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+flask --app fccapp run --host 0.0.0.0 --port 4000
+
