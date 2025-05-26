dfd07333640cb0131f628b6e125748fd00006a58
DeVerl Stokes <deverl.stokes@gmail.com>
2025-05-26 11:12:25 -0600

    Added a little logging.

--- bin/update.sh
+++ bin/update.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+echo -n "Starting at: "
+date
+
+echo "Fetching latest data..."
 bin/get.sh
 
+echo -n "Populating databases..."
+date
 bin/mkdbs.sh
 
+echo -n "Importing DMR data..."
+date
 bin/import_dmr.sh
 
+echo -n "Done at: "
+date
